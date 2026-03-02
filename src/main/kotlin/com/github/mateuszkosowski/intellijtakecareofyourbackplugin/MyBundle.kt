c975c487c51e6ea302eeaa077234c5127940cbf2
GitHub Action <action@github.com>
2026-03-02 19:38:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/mateuszkosowski/intellijtakecareofyourbackplugin/MyBundle.kt
+++ src/main/kotlin/com/github/mateuszkosowski/intellijtakecareofyourbackplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mateuszkosowski.intellijtakecareofyourbackplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mateuszkosowski/intellijtakecareofyourbackplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mateuszkosowski/intellijtakecareofyourbackplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.mateuszkosowski.intellijtakecareofyourbackplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mateuszkosowski.intellijtakecareofyourbackplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
